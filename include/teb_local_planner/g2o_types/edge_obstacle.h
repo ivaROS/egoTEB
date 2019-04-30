78f919e5dab1beec25c427c20e8aa18c4a35d934
Justin Smith <jssmith@gatech.edu>
2019-04-30 15:24:21 -0400

    Experimenting with approximate hard constraint for obstacles; causes system to behave unstably

--- include/teb_local_planner/g2o_types/edge_obstacle.h
+++ include/teb_local_planner/g2o_types/edge_obstacle.h
@@ -220,6 +220,14 @@ public:
     _error[0] = penaltyBoundFromBelow(dist, cfg_->obstacles.min_obstacle_dist, cfg_->optim.penalty_epsilon);
     _error[1] = penaltyBoundFromBelow(dist, cfg_->obstacles.inflation_dist, 0.0);
 
+    if(dist < cfg_->obstacles.min_obstacle_dist + cfg_->optim.penalty_epsilon)
+    {
+      _error[0] = 1/(dist - (cfg_->obstacles.min_obstacle_dist));
+    }
+    else
+    {
+      _error[0] = 0;
+    }
     
     ROS_ASSERT_MSG(std::isfinite(_error[0]) && std::isfinite(_error[1]), "EdgeObstacle::computeError() _error[0]=%f, _error[1]=%f\n",_error[0], _error[1]);
   }
